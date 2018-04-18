721f085b71541df8cb2143267c0b68a21c6253c9
Dongok Kang <dokang@fasoo.com>
2018-04-18 20:08:10 +0900

    Add: TODO: beautiful code printer

---
+++ src/main/scala/com/dev/pa/util/ASTToCode.scala
@@ -0,0 +1,10 @@
+package com.dev.pa.util
+
+import com.dev.pa.AST._
+
+object ASTToCode {
+// TODO: Print AST to beutiful Code
+//  def printProgram(program: Program, stringBuffer: StringBuffer): StringBuffer = {
+//
+//  }
+}
